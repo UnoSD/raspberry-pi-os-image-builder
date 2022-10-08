90da1c4dd5d864cc57e835f1aac9ad23452d8235
UnoSD <1582987+UnoSD@users.noreply.github.com>
2022-10-08 20:15:06 +0200

    Parameterize network

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
 touch /boot/ssh
-#echo "$USERNAME:PASSWORD" >> /boot/userconf.txt
+# Can we make it completely passwordless instead of a random 60 char passphrase?
+echo "$USERNAME:$(head -n 60 < /dev/urandom | tr -d '\n' | openssl passwd -6 -stdin)" >> /boot/userconf.txt
 mkdir /home/$USERNAME/.ssh
 chown $USERNAME:$USERNAME /home/$USERNAME/.ssh
 
 sed '/PasswordAuthentication/d' -i /etc/ssh/sshd_config
-echo >> /etc/ssh/sshd_config
-echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config
+echo -e "\nPasswordAuthentication no" >> /etc/ssh/sshd_config
 
 echo nameserver 1.1.1.1 > /etc/resolv.conf
 
@@ -21,5 +21,12 @@ chown $USERNAME:$USERNAME -R /home/$USERNAME/
 sed -i 's/^session[ \t]*optional[ \t]*pam_motd.so[ /t]*motd=\/run\/motd.dynamic/#session optional pam_motd.so motd=\/run\/motd.dynamic/' /etc/pam.d/login
 sed -i 's/^session[ \t]*optional[ \t]*pam_motd.so[ /t]*noupdate/#session optional pam_motd.so noupdate/' /etc/pam.d/login
 
-systemctl enable ssh
-systemctl enable getty@
+systemctl -q enable ssh
+systemctl -q enable getty@
+
+cat > /etc/dhcpd.conf <<EOF
+interface eth0
+static ip_address=$IP/$SUBNET
+static routers=$ROUTER
+static domain_name_servers=1.1.1.1 1.0.0.1
+EOF

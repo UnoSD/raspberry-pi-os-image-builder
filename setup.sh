c8c4e18f86cb03b8fe8f8ce829ca17d09c47fa08
UnoSD <1582987+UnoSD@users.noreply.github.com>
2022-10-09 03:49:25 +0200

    Refactor and make static IP optional

--- setup.sh
+++ setup.sh
@@ -15,27 +15,30 @@ deluser pi
 
 mkdir /home/$USERNAME/.ssh
 
-sed '/PasswordAuthentication/d' -i /etc/ssh/sshd_config
-echo -e "\nPasswordAuthentication no" >> /etc/ssh/sshd_config
+sed -i 's/[#]PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 
-echo nameserver 1.1.1.1 > /etc/resolv.conf
-
-apt-get -qq update
-apt-get -qqy upgrade
+apt-get -qq update && apt-get -qqy upgrade
 
 rm -f /etc/motd
 
 chown $USERNAME:$USERNAME -R /home/$USERNAME/
 
-sed -i 's/^session[ \t]*optional[ \t]*pam_motd.so[ /t]*motd=\/run\/motd.dynamic/#session optional pam_motd.so motd=\/run\/motd.dynamic/' /etc/pam.d/login
-sed -i 's/^session[ \t]*optional[ \t]*pam_motd.so[ /t]*noupdate/#session optional pam_motd.so noupdate/' /etc/pam.d/login
+sed -i '/^session[ \t]*optional[ \t]*pam_motd.so.*/d' /etc/pam.d/login
 
 systemctl -q enable ssh
-systemctl -q enable getty@
 
-cat > /etc/dhcpd.conf <<EOF
+if [[ -n ${IP} && -n ${SUBNET} && -n ${ROUTER} ]]; then
+
+    cat > /etc/dhcpd.conf <<EOF
 interface eth0
 static ip_address=$IP/$SUBNET
 static routers=$ROUTER
 static domain_name_servers=1.1.1.1 1.0.0.1
+
+interface wlan0
+static ip_address=$IP/$SUBNET
+static routers=$ROUTER
+static domain_name_servers=1.1.1.1 1.0.0.1
 EOF
+
+fi

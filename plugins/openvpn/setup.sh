eaefd788a684ddff2a0d68ccee47a440ad55d5e8
UnoSD <1582987+UnoSD@users.noreply.github.com>
2024-08-19 19:09:08 +0200

    Comment on VPN routes

--- plugins/openvpn/setup.sh
+++ plugins/openvpn/setup.sh
@@ -8,3 +8,5 @@ apt-get -qqy --no-install-recommends -o=Dpkg::Use-Pty=0 install openvpn
 # Set up OpenVPN
 mv /tmp/azure.conf /etc/openvpn/client/
 systemctl -q enable openvpn-client@azure.service
+
+# Is this using the VPN only for VPN routes??
